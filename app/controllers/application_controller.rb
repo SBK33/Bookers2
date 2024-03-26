b5ba7a119ccf4fe4283ef1c3f26159d0dcc675a3
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-03-26 15:38:01 +0000

    [update] sign in情報変更（devise設定ファイル・controllerのストロングパラメーターの変更)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:email])
+    #上記のkeysをnameのままにして、deviseの設定ファイルを初期設定からconfig.authentication_keys = [:name]に変更した結果、signin時emailの入力を受け付けなくなった。
+    #上記の記述は、受け付けるカラムの許可をしているのであれば、初期設定デフォルトのまま、上記keysをnameのままでもname+パスワードでのログイン聞いたのでは？
   end
 
 end
