419555024d6047766a765ba7d1249211ef82c503
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-03-30 14:43:06 +0000

    [add] book updateの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,6 +14,12 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book.id)
+  end
+
   def index
     @books = Book.all
   end
@@ -21,7 +27,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])
   end
-  
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
