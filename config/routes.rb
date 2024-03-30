690e670da79e863f7bc8e3ab3eda46d6f599c787
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-03-30 09:42:47 +0000

    [update] ログイン時のリンク先を設定,ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show, :edit, :destroy]
+  devise_for :users
+  resources :users, only: [:index, :show, :edit, :destroy, :update]
   resources :books, only: [:new, :index, :show , :create ,:destroy]
   #URL名は確認すること
+  #usersに対するルーティングの重複
 
-  devise_for :users
+
   root to: "homes#top"
   get "home/about" => 'homes#about', as: :home_about
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
