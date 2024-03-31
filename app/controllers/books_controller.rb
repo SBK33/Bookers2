e325b17f2fe369134ac61348d4164b84804eea3f
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-03-31 14:31:37 +0000

    [update]バリデーション設定：新規book作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,9 +6,16 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    flash[:notice] = "You have created book successfully."
-    redirect_to book_path(@book.id)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      @book_new = Book.new
+      @user = current_user
+      render :index
+    end
+
   end
 
   def edit
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,3 +1,11 @@
 class Book < ApplicationRecord
     belongs_to :user
+
+    validates :title, presence: true
+    validates :body,
+      presence: true,
+      length: { maximum: 200 }
 end
+
+#title：空でないように設定してください。
+#body：空でない、かつ最大200文字までに設定してください。
