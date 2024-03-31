15545e10d29ca5aefaac281a32ec1a1b40bbc6db
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-03-31 13:53:28 +0000

    [update]フラッシュメッセージの実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,9 +16,11 @@ class UsersController < ApplicationController
   def edit
     @user = User.find(params[:id])
   end
+  
   def update
     @user = User.find(params[:id])
     @user.update(user_params)
+    flash[:notice] = "You have updated user successfully."
     redirect_to user_path(@user.id)
   end
 
