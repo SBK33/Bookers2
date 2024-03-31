e5b1e7b0b361eb1a6a3ae818284b26029bc3cc26
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-03-31 03:17:45 +0000

    [update]routes 修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :users, only: [:index, :show, :edit, :update, :destroy]
+  resources :users, only: [:index, :show, :edit, :update]
   resources :books, only: [:index, :show , :create, :edit, :update, :destroy]
   #URL名は確認すること
   #usersに対するルーティングの重複
 
-
+  
   root to: "homes#top"
   get "home/about" => 'homes#about', as: :home_about
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
