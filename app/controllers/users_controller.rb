06999a1f8b18bc0ba1b2a4308e7879006d160a2e
SBK33 <157976694+SBK33@users.noreply.github.com>
2024-04-01 15:11:13 +0000

    [update]認証設定：user情報の編集制限

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :is_matching_login_user, only: [:edit, :update]
+  #updateも設定する理由
   def index
     @users = User.all
     #共通フォーマット向け記述
@@ -31,4 +33,10 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:name, :profile_image, :introduction)
     end
+  def is_matching_login_user
+    user = User.find(params[:id])
+    unless user.id == current_user.id
+      redirect_to books_path
+    end
+  end
 end
